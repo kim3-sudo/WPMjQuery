f3de5c35506aae8383ef5aced47ecfc1a1cc12b2
Sejin Kim <54730100+kim3-sudo@users.noreply.github.com>
2019-09-25 00:52:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
 	var timeStart;
 	var errorCount = 0;
 
-	$wpmPrompt.text("So she went into the garden to cut a cabbage leaf to make an apple pie; and at the same time a great she-bear coming up the street pops its head into the sho. "What! No soap? So he died, and she very imprudently married the barber; and there were present the Picninnies, and the Grand Panjandrum himself, with the little round button at the top, and they all fell to playing the game of catch as catch can, till the gunpowder ran out at the heels of their boots.");
+	$wpmPrompt.text("So she went into the garden to cut a cabbage leaf to make an apple pie; and at the same time a great she-bear coming up the street pops its head into the shop. "What! No soap? So he died, and she very imprudently married the barber; and there were present the Picninnies, and the Grand Panjandrum himself, with the little round button at the top, and they all fell to playing the game of catch as catch can, till the gunpowder ran out at the heels of their boots.");
 	var $wpmPromptLength = $wpmPrompt.text().length;
 
 	// configure accuracy forumla
