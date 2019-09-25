5d59270cd8cc7934210c9f6069f47d629aac8823
Sejin Kim <54730100+kim3-sudo@users.noreply.github.com>
2019-09-24 23:53:11 -0400

    Update script.js

--- script.js
+++ script.js
@@ -11,11 +11,32 @@ $(document).ready(function(){
 	var food;
 	var score;
 	
+	function rainbow(numOfSteps, step) {
+	    // This function generates vibrant, "evenly spaced" colours (i.e. no clustering). This is ideal for creating easily distinguishable vibrant markers in Google Maps and other apps.
+	    // Adam Cole, 2011-Sept-14
+	    // HSV to RBG adapted from: http://mjijackson.com/2008/02/rgb-to-hsl-and-rgb-to-hsv-color-model-conversion-algorithms-in-javascript
+	    var r, g, b;
+	    var h = step / numOfSteps;
+	    var i = ~~(h * 6);
+	    var f = h * 6 - i;
+	    var q = 1 - f;
+	    switch(i % 6){
+		case 0: r = 1; g = f; b = 0; break;
+		case 1: r = q; g = 1; b = 0; break;
+		case 2: r = 0; g = 1; b = f; break;
+		case 3: r = 0; g = q; b = 1; break;
+		case 4: r = f; g = 0; b = 1; break;
+		case 5: r = 1; g = 0; b = q; break;
+	    }
+	    var c = "#" + ("00" + (~ ~(r * 255)).toString(16)).slice(-2) + ("00" + (~ ~(g * 255)).toString(16)).slice(-2) + ("00" + (~ ~(b * 255)).toString(16)).slice(-2);
+	    return (c);
+	}
+	
 	//Lets create the snake now
 	var snake_array; //an array of cells to make up the snake
 	
 	function init() {
-		d = "right"; //default direction
+		d = "down"; //default direction
 		create_snake();
 		create_food(); //Now we can see the food particle
 		//finally lets display the score
@@ -112,7 +133,7 @@ $(document).ready(function(){
 	
 	// Lets first create a generic function to paint cells
 	function paint_cell(x, y) {
-		ctx.fillStyle = "blue";
+		ctx.fillStyle = "rainbow";
 		ctx.fillRect(x*cw, y*cw, cw, cw);
 		ctx.strokeStyle = "white";
 		ctx.strokeRect(x*cw, y*cw, cw, cw);
