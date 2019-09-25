91d14950159559491b899f60180adff71700872a
Sejin Kim <54730100+kim3-sudo@users.noreply.github.com>
2019-09-25 00:02:23 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,160 +1,157 @@
+// The base speed per character
+time_setting = 30;
+// How much to 'sway' (random * this-many-milliseconds)
+random_setting = 100;
+// The text to use NB use \n not real life line breaks!
+input_text = "How fast can you type?";
+// Where to fill up
+target_setting = $("#output");
+// Launch that function!
+type(input_text, target_setting, 0, time_setting, random_setting);
+
+function type(input, target, current, time, random){
+  // If the current count is larger than the length of the string, then for goodness sake, stop
+	if(current > input.length){
+    // Write Complete
+		console.log("Complete.");
+	}
+	else{
+	 // console.log(current)
+    // Increment the marker
+		current += 1;
+    // fill the target with a substring, from the 0th character to the current one
+		target.text(input.substring(0,current));
+    // Wait ...
+		setTimeout(function(){
+      // do the function again, with the newly incremented marker
+			type(input, target, current, time, random);
+      // Time it the normal time, plus a random amount of sway
+		},time + Math.random()*random);
+	}
+}
+
+/*
+ * The typing test stuff
+ */
+
+var character_length = 31;
+var index = 0;
+var letters =  $("#input_text").val();
+var started = false;
+var current_string = letters.substring(index, index + character_length);
+
+var wordcount = 0;
+
+$("html, body").click(function(){
+  $("#textarea").focus();
+});
+
+$("#target").text(current_string);
+$(window).keypress(function(evt){
+  if(!started){
+    start();
+    started = true;
+  }
+  evt = evt || window.event;
+  var charCode = evt.which || evt.keyCode;
+  var charTyped = String.fromCharCode(charCode);
+  if(charTyped == letters.charAt(index)){
+    if(charTyped == " "){
+      wordcount ++;
+      $("#wordcount").text(wordcount);
+    }
+    index ++;
+    current_string = letters.substring(index, index + character_length);
+    $("#target").text(current_string);
+    $("#your-attempt").append(charTyped);
+    if(index == letters.length){
+      wordcount ++;
+      $("#wordcount").text(wordcount);
+      $("#timer").text(timer);
+      if(timer == 0){
+        timer = 1;
+      }
+      wpm = Math.round(wordcount / (timer / 60));
+      $("#wpm").text(wpm);
+      stop();
+      finished();
+    }
+  }else{
+    $("#your-attempt").append("<span class='wrong'>" + charTyped + "</span>");
+    errors ++;
+    $("#errors").text(errors);
+  }
+});
+
+var timer = 0;
+var wpm = 0;
+var errors = 0;
+var interval_timer;
+
+$("#reset").click(function(){
+  reset();
+});
+
+$("#change").click(function(){
+  $("#input_text").show().focus();
+});
+
+$("#pause").click(function(){
+  stop();
+});
+
+$("#input_text").change(function(){
+  reset();
+});
+
+function start(){
+  interval_timer = setInterval(function(){
+    timer ++;
+    $("#timer").text(timer);
+    wpm = Math.round(wordcount / (timer / 60));
+    $("#wpm").text(wpm);
+  }, 1000)
+}
+
+function stop(){
+  clearInterval(interval_timer);
+  started = false;
+}
+
+function reset(){
+  $("#input_text").blur().hide();;
+  $("#your-attempt").text("");
+  index = 0;
+  errors = 0;
+  clearInterval(interval_timer);
+  started = false;
+  letters = $("#input_text").val();
+  $("#wpm").text("0");
+  $("#timer").text("0");
+  $("#wordcount").text("0");
+  timer = 0;
+  wpm = 0;
+  current_string = letters.substring(index, index + character_length);
+  $("#target").text(current_string);
+}
+
+function finished(){
+  alert("Congratulations!\nWords per minute: " + wpm + "\nWordcount: " + wordcount + "\nErrors:" + errors);
+}
+
+var window_focus;
+
+$(window).focus(function() {
+    window_focus = true;
+}).blur(function() {
+  window_focus = false;
+});
+
 $(document).ready(function(){
-	//Canvas stuff
-	var canvas = $("#canvas")[0];
-	var ctx = canvas.getContext("2d");
-	var w = $("#canvas").width();
-	var h = $("#canvas").height();
-	
-	//Lets save the cell width in a variable for easy control
-	var cw = 10;
-	var d;
-	var food;
-	var score;
-	
-	function rainbow(numOfSteps, step) {
-	    // This function generates vibrant, "evenly spaced" colours (i.e. no clustering). This is ideal for creating easily distinguishable vibrant markers in Google Maps and other apps.
-	    // Adam Cole, 2011-Sept-14
-	    // HSV to RBG adapted from: http://mjijackson.com/2008/02/rgb-to-hsl-and-rgb-to-hsv-color-model-conversion-algorithms-in-javascript
-	    var r, g, b;
-	    var h = step / numOfSteps;
-	    var i = ~~(h * 6);
-	    var f = h * 6 - i;
-	    var q = 1 - f;
-	    switch(i % 6){
-		case 0: r = 1; g = f; b = 0; break;
-		case 1: r = q; g = 1; b = 0; break;
-		case 2: r = 0; g = 1; b = f; break;
-		case 3: r = 0; g = q; b = 1; break;
-		case 4: r = f; g = 0; b = 1; break;
-		case 5: r = 1; g = 0; b = q; break;
-	    }
-	    var c = "#" + ("00" + (~ ~(r * 255)).toString(16)).slice(-2) + ("00" + (~ ~(g * 255)).toString(16)).slice(-2) + ("00" + (~ ~(b * 255)).toString(16)).slice(-2);
-	    return (c);
-	}
-	
-	//Lets create the snake now
-	var snake_array; //an array of cells to make up the snake
-	
-	function init() {
-		d = "down"; //default direction
-		create_snake();
-		create_food(); //Now we can see the food particle
-		//finally lets display the score
-		score = 0;
-		
-		//Lets move the snake now using a timer which will trigger the paint function
-		//every 60ms
-		if(typeof game_loop != "undefined") clearInterval(game_loop);
-		game_loop = setInterval(paint, 60);
-	}
-	init();
-	
-	function create_snake() {
-		var length = 5; //Length of the snake
-		snake_array = []; //Empty array to start with
-		for(var i = length-1; i>=0; i--) {
-			//This will create a horizontal snake starting from the top left
-			snake_array.push({x: i, y:0});
-		}
-	}
-	
-	//Lets create the food now
-	function create_food() {
-		food = {
-			x: Math.round(Math.random()*(w-cw)/cw), 
-			y: Math.round(Math.random()*(h-cw)/cw), 
-		};
-		//This will create a cell with x/y between 0-44
-		//Because there are 45(450/10) positions accross the rows and columns
-	}
-	
-	//Lets paint the snake now
-	function paint() {
-		//To avoid the snake trail we need to paint the BG on every frame
-		//Lets paint the canvas now
-		ctx.fillStyle = "white";
-		ctx.fillRect(0, 0, w, h);
-		ctx.strokeStyle = "black";
-		ctx.strokeRect(0, 0, w, h);
-		
-		//The movement code for the snake to come here.
-		//The logic is simple
-		//Pop out the tail cell and place it infront of the head cell
-		var nx = snake_array[0].x;
-		var ny = snake_array[0].y;
-		//These were the position of the head cell.
-		//We will increment it to get the new head position
-		//Lets add proper direction based movement now
-		if(d == "right") nx++;
-		else if(d == "left") nx--;
-		else if(d == "up") ny--;
-		else if(d == "down") ny++;
-		
-		//Lets add the game over clauses now
-		//This will restart the game if the snake hits the wall
-		//Lets add the code for body collision
-		//Now if the head of the snake bumps into its body, the game will restart
-		if(nx == -1 || nx == w/cw || ny == -1 || ny == h/cw || check_collision(nx, ny, snake_array)) {
-			//restart game
-			init();
-			//Lets organize the code a bit now.
-			return;
-		}
-		
-		//Lets write the code to make the snake eat the food
-		//The logic is simple
-		//If the new head position matches with that of the food,
-		//Create a new head instead of moving the tail
-		if(nx == food.x && ny == food.y) {
-			var tail = {x: nx, y: ny};
-			score++;
-			//Create new food
-			create_food();
-		} else {
-			var tail = snake_array.pop(); //pops out the last cell
-			tail.x = nx; tail.y = ny;
-		}
-		//The snake can now eat the food.
-		
-		snake_array.unshift(tail); //puts back the tail as the first cell
-		
-		for(var i = 0; i < snake_array.length; i++) {
-			var c = snake_array[i];
-			//Lets paint 10px wide cells
-			paint_cell(c.x, c.y);
-		}
-		
-		//Lets paint the food
-		paint_cell(food.x, food.y);
-		//Lets paint the score
-		var score_text = "Score: " + score;
-		ctx.fillText(score_text, 5, h-5);
-	}
-	
-	// Lets first create a generic function to paint cells
-	function paint_cell(x, y) {
-		ctx.fillStyle = "rainbow";
-		ctx.fillRect(x*cw, y*cw, cw, cw);
-		ctx.strokeStyle = "white";
-		ctx.strokeRect(x*cw, y*cw, cw, cw);
-	}
-	
-	function check_collision(x, y, array) {
-		//This function will check if the provided x/y coordinates exist
-		//in an array of cells or not
-		for(var i = 0; i < array.length; i++) {
-			if(array[i].x == x && array[i].y == y)
-			 return true;
-		}
-		return false;
-	}
-	// Accept HID inputs
-	$(document).keydown(function(e){
-		var key = e.which;
-		//We will add another clause to prevent reverse gear
-		if(key == "37" && d != "right") d = "left";
-		else if(key == "38" && d != "down") d = "up";
-		else if(key == "39" && d != "left") d = "right";
-		else if(key == "40" && d != "up") d = "down";
-	})
-})
+  if(window_focus){
+    $("#focus").hide();
+  }
+  $(window).focus(function() {
+    $("#focus").hide();
+  });
+});
